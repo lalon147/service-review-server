fd0a2c6bb139d0fb173e5f87f3cbf51839c86dae
lalon147 <lalon.rahman147@gmail.com>
2022-11-08 04:25:09 +0600

    JSON web token, services added

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors');
+require(`dotenv`).config();
+const jwt=require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app=express()
+const port =process.env.PORT  || 5000;
+
+//middle ware 
+app.use(cors());
+app.use(express.json())
+
+                 //TESTING PURPOSE 
+                 app.get("/test",(req,res)=>{
+                    res.send("THIS SERVER IS RUNNING")
+                   })
+
+
+                   //database configaration 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gw8hef2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const database=client.db("food-master");
+const serviceCollection=database.collection("services");
+
+
+///VERIFYING THAT THE USER HAVE THE RIGHT OR AUTHENTIC JSONWEBTOKEN
+const verifyJwt=(req,res,next)=>{
+    // console.log(req.body)
+    const authHeader=req.headers.authorization;
+    // console.log(authHeader);
+    if(!authHeader){
+        return res.status(401).send({message:"UNAUTHORIZED USER"})
+    }
+    const token=authHeader.split(" ")[1];
+    // console.log(token)
+    jwt.verify(token,process.env.ACCESS_TOKEN,function(err,decoded){
+        if(err){
+            return res.status(401).send({message:"UNAUTHORIZED ACCESS"})
+        }
+        req.decoded=decoded;
+        next()
+    })
+}
+                   
+
+const  run=async()=>{
+                         try{
+                             app.get("/services",async(req,res)=>{ 
+                                const count=serviceCollection.estimatedDocumentCount();                            
+                                let size=3
+                                if(req.query.size==="all"){
+                                   size=count   
+                                }   
+                                
+                                const cursor= serviceCollection.find({})
+                                const services=await cursor.limit(size).toArray()                 
+                                res.send(services)
+                            
+                                
+                             })
+                             app.post("/jwt",(req,res)=>{
+                                
+                                const user=req.body;
+                                console.log(user);
+                                const token=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:"1h"})
+                                res.send({token})
+                             })
+                             
+                             app.post("/add-a-service",verifyJwt,async(req,res)=>{
+                                // console.log(req.decoded);
+                                const movie=req.body;
+                                console.log(movie);
+                                const result=await moviesCollection.insertOne(movie);
+                                res.send(movie);
+                             })
+                            
+
+                         }
+                         finally{
+
+                         }
+} 
+run().catch(error=>console.log(error))
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`SERVER IS RUNNING ON ${port}`)
+} )
